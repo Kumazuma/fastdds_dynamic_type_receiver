e933567f415d23bac801d865cfe55af0c2051db9
Kumazuma <qweads12@gmail.com>
2023-09-13 23:19:08 +0900

    feature what saves octet sequence as a file

--- src/widget/sequence_view_dialog.cpp
+++ src/widget/sequence_view_dialog.cpp
@@ -1,5 +1,11 @@
 #include "sequence_view_dialog.h"
 #include <wx/artprov.h>
+#include <wx/file.h>
+
+BEGIN_EVENT_TABLE(SequenceViewDialog<uint8_t>, wxDialog)
+	EVT_TOOL(wxID_SAVE, SequenceViewDialog::OnCommandSaveFile)
+	EVT_TOOL(wxID_OPEN, SequenceViewDialog::OnCommandOpenAsImage)
+END_EVENT_TABLE()
 
 SequenceViewDialog<uint8_t>::SequenceViewDialog(wxWindow* parent, wxWindowID id, const wxString& title) :
 wxDialog(parent, id, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER)
@@ -14,7 +20,7 @@ wxDialog(parent, id, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_S
 										wxArtProvider::GetBitmapBundle(wxART_FILE_SAVE, wxART_TOOLBAR));
 	pToolSave->SetShortHelp(wxS("Save binary as file"));
 	auto pToolOpenAs = m_pToolbar->AddTool(wxID_OPEN, wxS("Try show as a image"),
-											wxArtProvider::GetBitmapBundle(wxART_FILE_OPEN, wxART_TOOLBAR));
+											wxArtProvider::GetBitmapBundle(wxART_NORMAL_FILE, wxART_TOOLBAR));
 	pToolOpenAs->SetShortHelp(wxS("Try decode a image and show it"));
 	m_pToolbar->Realize();
 	auto bsizer = new wxBoxSizer(wxVERTICAL);
@@ -27,4 +33,50 @@ void SequenceViewDialog<uint8_t>::SetValue(const ReadOnlySequence<uint8_t>& valu
 {
 	m_value = value;
 	m_pHexViewCtrl->SetValue(value);
+}
+
+void SequenceViewDialog<unsigned char>::OnCommandSaveFile(wxCommandEvent& evt)
+{
+	if(m_value.size() == 0)
+	{
+		return;
+	}
+
+	wxFileDialog fileDialog(this, wxS("Save binary as a file"), wxEmptyString, wxEmptyString, wxS("*.*|*.*"), wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
+	auto ret = fileDialog.ShowModal();
+	if(ret != wxID_OK)
+	{
+		return;
+	}
+
+	auto path = fileDialog.GetPath();
+	wxFile file;
+	if(!file.Open(path, wxFile::write))
+	{
+		wxMessageDialog diloag(this, wxS("Failed to open file"), wxMessageBoxCaptionStr, wxOK | wxCENTER | wxICON_ERROR);
+		diloag.CenterOnParent();
+		diloag.ShowModal();
+		return;
+	}
+
+	auto& binary = m_value.Get();
+	size_t offset = 0;
+	while(binary.size() != offset)
+	{
+		auto written = file.Write(binary.data() + offset, binary.size() - offset);
+		if(file.Error())
+		{
+			wxMessageDialog diloag(this, wxS("Failed to write to file"), wxMessageBoxCaptionStr, wxOK | wxCENTER | wxICON_ERROR);
+			diloag.CenterOnParent();
+			diloag.ShowModal();
+			return;
+		}
+
+		offset += written;
+	}
+}
+
+void SequenceViewDialog<unsigned char>::OnCommandOpenAsImage(wxCommandEvent& evt)
+{
+	
 }
